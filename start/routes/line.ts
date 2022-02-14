94c55f660bd173a1893d3413e862d5f2e8138d01
Tiago Dias <tiagojpdias@gmail.com>
2022-02-14 16:27:24 +0000

    refactor: promote line routes matcher

--- start/routes/line.ts
+++ start/routes/line.ts
@@ -5,10 +5,11 @@ const lineMatcher = RegExp(`\\b(${Object.keys(LineMapping).join('|')})\\b`);
 
 Route.group(() => {
   Route.get('/', 'LinesController.index');
-  Route.get('/:line', 'LinesController.show').where('line', lineMatcher);
-  Route.get('/:line/waitingTimes', 'LinesController.waitingTimes').where('line', lineMatcher);
+  Route.get('/:line', 'LinesController.show');
+  Route.get('/:line/waitingTimes', 'LinesController.waitingTimes');
   Route.get('/:line/frequency/:day/:hour?', 'LinesController.frequency')
-    .where('line', lineMatcher)
     .where('day', /^(S|s|F|f)$/)
     .where('hour', /^([01][0-9]|2[0-3])([0-5][0-9]){2}$/);
-}).prefix('/lines');
+})
+  .prefix('/lines')
+  .where('line', lineMatcher);
